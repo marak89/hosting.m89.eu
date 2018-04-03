751a056f7a12cdafe3b5f5b4da62c087aa9331fd
Marcin Rał <marcin@ral.info.pl>
2018-04-03 20:34:09 +0200

    add section wss web socket secure fix error 400

--- cert.sh
+++ cert.sh
@@ -112,6 +112,11 @@ nginxConf="$nginxConf      proxy_set_header        X-Real-IP \$remote_addr;\n"
 nginxConf="$nginxConf      proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;\n"
 nginxConf="$nginxConf      proxy_set_header        X-Forwarded-Proto \$scheme;\n\n"
 
+nginixConf="# wss web socket secure fix error 400"
+nginixConf="proxy_set_header Upgrade $http_upgrade;"
+nginixConf="proxy_set_header Connection \"Upgrade\";"
+nginixConf="proxy_http_version 1.1;"
+
 nginxConf="$nginxConf      proxy_ssl_session_reuse off;\n\n"
 
 nginxConf="$nginxConf      # Fix the It appears that your reverse proxy set up is broken error.\n"
