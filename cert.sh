8386060d57ca884794805317529ca6da5670a276
Marcin Rał <marcin@ral.info.pl>
2018-04-18 17:32:47 +0200

    poprawnie dodany naglowek dla web socket secure

--- cert.sh
+++ cert.sh
@@ -111,11 +111,10 @@ nginxConf="$nginxConf      proxy_set_header        Host \$host;\n"
 nginxConf="$nginxConf      proxy_set_header        X-Real-IP \$remote_addr;\n"
 nginxConf="$nginxConf      proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;\n"
 nginxConf="$nginxConf      proxy_set_header        X-Forwarded-Proto \$scheme;\n\n"
-
-nginxConf="$nginixConf	   # wss web socket secure fix error 400"
-nginxConf="$nginixConf	   proxy_set_header	Upgrade $http_upgrade;"
-nginxConf="$nginixConf	   proxy_set_header	Connection \"Upgrade\";"
-nginxConf="$nginixConf	   proxy_http_version	 1.1;"
+nginxConf="$nginxConf     #wss web socket secure fix error 400\n"
+nginxConf="$nginxConf     proxy_set_header	Upgrade \$http_upgrade;\n"
+nginxConf="$nginxConf     proxy_set_header	Connection "\""Upgrade"\"";\n"
+nginxConf="$nginxConf     proxy_http_version	 1.1;\n"
 
 nginxConf="$nginxConf      proxy_ssl_session_reuse off;\n\n"
 
